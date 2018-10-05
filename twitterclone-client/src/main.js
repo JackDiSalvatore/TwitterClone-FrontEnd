7ba0ac18256b95d4d6e9867d9eed8fe2c29b75d9
jackdisalvatore <jackp.disalvatore@gmail.com>
2018-10-05 18:11:54 -0400

    adding boostrap vue

--- twitterclone-client/src/main.js
+++ twitterclone-client/src/main.js
@@ -5,9 +5,13 @@ import App from './App'
 import Router from './router'
 import VueResource from 'vue-resource'
 import Auth from './plugins/Auth.js'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
 Vue.use(VueResource)
 Vue.use(Auth)
 
