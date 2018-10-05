7d6556f570c70aebf4f7812ab19c993e58f11bd8
jackdisalvatore <jackp.disalvatore@gmail.com>
2018-10-05 18:10:52 -0400

    removing logo

--- twitterclone-client/src/components/auth/Auth.vue
+++ twitterclone-client/src/components/auth/Auth.vue
@@ -1,16 +1,21 @@
 <template>
-   <div id="auth">
-      <div class="container">
-         <div class="row">
-            <div class="col authForum">
-               <h2 class="text-center">Twitter</h2>
-               <p></p>
-               <transition name="fade">
-                  <router-view></router-view>
-               </transition>
+   <div>
+      <img src="@/assets/Twitter_Logo_Blue.png" class="m-b-20 m-t-20">
+
+      <div id="auth">
+         <div class="container">
+            <div class="row">
+               <div class="col authForum">
+                  <h2 class="text-center">Twitter</h2>
+                  <p></p>
+                  <transition name="fade">
+                     <router-view></router-view>
+                  </transition>
+               </div>
             </div>
          </div>
       </div>
+
    </div>
 </template>
 
@@ -22,6 +27,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+img {
+  text-align: center;
+  width: 150px;
+  height: 150px;
+}
+
 #auth {
    height: 100%;
    display: flex;
